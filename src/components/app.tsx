676b6ab5eb4436cc53498cc6a2a709ec634137d9
Matt Schiffman <matt@Matts-MacBook-Pro.local>
2020-01-06 16:54:04 -0500

    formatting

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,47 +1,47 @@
 import React from 'react';
 
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
+import {ApolloClient} from 'apollo-client';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {HttpLink} from 'apollo-link-http';
 import {ApolloProvider} from '@apollo/react-hooks';
 
 import SampleQuery from "./sample-query";
 
 interface AppProps {
-    compiler: string;
-    framework: string;
+  compiler: string;
+  framework: string;
 }
 
 const App = (props: AppProps) => {
 
-    const cache = new InMemoryCache();
-    const link = new HttpLink({
-        uri: 'http://localhost:4000/',
-    });
+  const cache = new InMemoryCache();
+  const link = new HttpLink({
+    uri: 'http://localhost:4000/',
+  });
 
 
-    const client = new ApolloClient({
-        // Provide required constructor fields
-        cache: cache,
-        link: link,
+  const client = new ApolloClient({
+    // Provide required constructor fields
+    cache: cache,
+    link: link,
 
-        // Provide some optional constructor fields
-        name: 'react-web-client',
-        version: '1.3',
-        queryDeduplication: false,
-        defaultOptions: {
-            watchQuery: {
-                fetchPolicy: 'cache-and-network',
-            },
-        }
-    });
+    // Provide some optional constructor fields
+    name: 'react-web-client',
+    version: '1.3',
+    queryDeduplication: false,
+    defaultOptions: {
+      watchQuery: {
+        fetchPolicy: 'cache-and-network',
+      },
+    }
+  });
 
-    return (
-        <ApolloProvider client={client}>
-            <h1>Hello from {props.compiler} and {props.framework}!</h1>
-            <SampleQuery />
-        </ApolloProvider>
-    );
+  return (
+    <ApolloProvider client={client}>
+      <h1>Hello from {props.compiler} and {props.framework}!</h1>
+      <SampleQuery/>
+    </ApolloProvider>
+  );
 };
 
 export default App;
--- src/components/sample-query.tsx
+++ src/components/sample-query.tsx
@@ -1,31 +1,31 @@
 import React from 'react'
-import { useQuery } from '@apollo/react-hooks';
-import { SAMPLE_QUERY } from "../graphql/queries";
+import {useQuery} from '@apollo/react-hooks';
+import {SAMPLE_QUERY} from "../graphql/queries";
 
 interface Link {
-    id: string;
-    description: string;
-    url: string;
+  id: string;
+  description: string;
+  url: string;
 }
 
 const SampleQuery = () => {
 
-    const { loading, error, data } = useQuery(SAMPLE_QUERY);
+  const {loading, error, data} = useQuery(SAMPLE_QUERY);
 
-    if (loading) return <p>Loading...</p>;
+  if (loading) return <p>Loading...</p>;
 
-    return (<ul>
-          {data && data.feed.links.map((link:Link, idx:number) => {
-              return (
-                <li key={idx}>
-                    <span>{link.id}</span>
-                    <span> : </span>
-                    <span>{link.description}</span>
-                    <div>{link.url}</div>
-                </li>
-              );
-          })}
-      </ul>)
+  return (<ul>
+    {data && data.feed.links.map((link: Link, idx: number) => {
+      return (
+        <li key={idx}>
+          <span>{link.id}</span>
+          <span> : </span>
+          <span>{link.description}</span>
+          <div>{link.url}</div>
+        </li>
+      );
+    })}
+  </ul>)
 };
 
 export default SampleQuery;
